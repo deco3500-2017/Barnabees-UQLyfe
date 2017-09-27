24122ad041651f985f14449d4a503113badd4e4e
BigWilfred <willyduguid@gmail.com>
2017-09-27 12:45:36 +1000

    Added food and sport for added proof of concept

--- Website/ProfileProto/script.js
+++ Website/ProfileProto/script.js
@@ -2,7 +2,7 @@ var createdEvent = {"eventName":"", "eventTime" : 0, "eventDay" : 0, "eventMonth
 
 //this is a test to understand the profile system with badges. not 100% that this is the best method, however we need to use an int to add show progress
 var person1 = {'name':'John Wick', 'photo':'path_to_photo', 'level':0, 'badges':{
-	'sport':5,
+	'sport':0,
 	'food':150,
 	'misc':0,
 	'faculty':0,
@@ -14,12 +14,14 @@ var person1 = {'name':'John Wick', 'photo':'path_to_photo', 'level':0, 'badges':
 	}
 };
 var check= checkBadgeColour(person1, 'sport', false, 200, 100, 40);
-
+var check2= checkBadgeColour(person1, 'food', false, 200, 100, 40);
 $(document).ready(function(){
 	
 	
 	console.log(check);
-	$('#displayPerson').append('This is an example of what the current structure of the person JSON is. <br>Name: ' + person1.name + '<br> Badge for sport: ' + check[0] + '<br>Progress: ' + check[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+check[2][0] + '<br>Current: ' + check[2][1] + '<br>Low Point: ' +check[2][2]);
+	//$('#displayPerson').append('This is an example of what the current structure of the person JSON is. <br>Name: ' + person1.name + '<br> Badge for sport: ' + check[0] + '<br>Progress: ' + check[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+check[2][0] + '<br>Current: ' + check[2][1] + '<br>Low Point: ' +check[2][2]);
+	quickDisplay(check, '#displayPerson', 'SPORT');
+	quickDisplay(check2, '#displayPerson2', 'FOOD');
 	
 });
 
@@ -42,12 +44,30 @@ $('#addSport').submit(function(event){
 		$('#displayPerson').empty();
 		
 		check= checkBadgeColour(person1, 'sport', false, 200, 100, 40);
+		 quickDisplay(check, '#displayPerson', 'SPORT');
 		
-		$('#displayPerson').append('This is an example of what the current structure of the person JSON is. <br>Name: ' + person1.name + '<br> Badge for sport: ' + check[0] + '<br>Progress: ' + check[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+check[2][0] + '<br>Current: ' + check[2][1] + '<br>Low Point: ' +check[2][2]);
+		//$('#displayPerson').append('This is an example of what the current structure of the person JSON is. <br>Name: ' + person1.name + '<br> Badge for sport: ' + check[0] + '<br>Progress: ' + check[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+check[2][0] + '<br>Current: ' + check[2][1] + '<br>Low Point: ' +check[2][2]);
 	}
 	
 })
-
+$('#addFood').submit(function(event){
+	event.preventDefault();
+	
+	if(isNaN($('#numberFood').val()/2) == false){
+		var addValue = parseInt($('#numberFood').val());
+		console.log(addValue);
+		
+		//add to badge
+		addBadge(person1, 'food', false, addValue);
+		
+		$('#displayPerson2').empty();
+		
+		check2= checkBadgeColour(person1, 'food', false, 200, 100, 40);
+		quickDisplay(check2, '#displayPerson2', 'FOOD');
+		//$('#displayPerson').append('This is an example of what the current structure of the person JSON is. <br>Name: ' + person1.name + '<br> Badge for sport: ' + check[0] + '<br>Progress: ' + check[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+check[2][0] + '<br>Current: ' + check[2][1] + '<br>Low Point: ' +check[2][2]);
+	}
+	
+})
 //used to check a badge status. gold etc refer to the level the badge must be on to recieve badge
 function checkBadgeColour(user, badge, unique, gold, silver, bronze){
 	var currentScore = 0;
@@ -118,4 +138,8 @@ function addBadge(user, badge, unique, amount){
 //((currentScore - bronze)/(silver - bronze)) * 100;
 function calculatePercentage(score, high, low){
 	return ((score - low)/(high-low))*100;
+}
+
+function quickDisplay(results, where, badge){
+	$(where).append('<br><br><br>Name: ' + person1.name + '<br> Badge for '+badge+': ' + results[0] + '<br>Progress: ' + results[1] + '<br><br>THESE WOULD BE USED FOR DISPLAY <br> High Point: '+results[2][0] + '<br>Current: ' + results[2][1] + '<br>Low Point: ' +results[2][2]);
 }
