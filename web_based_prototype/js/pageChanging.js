77b07ce0cd439ca78fcfb62ac35c5c95773264aa
Tyler Beutel <tylerbeutel97@gmail.com>
2017-10-10 14:23:12 +1000

    Makes sure a page attribute exists before changing page + Home functionality in index.html

--- web_based_prototype/js/pageChanging.js
+++ web_based_prototype/js/pageChanging.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){    
     
+    // When you click a 'button' with a 'page' attribute
+    //   set the src of the 'mainFrame' to the value.
     $("#mainFrame").on( "load", function() {
         $("#mainFrame").contents().on("click", "button", function() {
-            $("#mainFrame").attr("src", $(this).attr("page") );
+            if ( $(this).attr("page") != undefined) {
+                $("#mainFrame").attr("src", $(this).attr("page") );
+            }
         });
     });
     
+    // When you click the 'home' button in index.html
     $("#home").click(function(){
         $("#mainFrame").attr("src", "home.html" );
     })
