8dc431921ef89464912fb9380538ed4c2c6f3167
BigWilfred <willyduguid@gmail.com>
2017-10-18 16:30:42 +1000

    Working on homepage to to display close events

--- web_based_prototype/js/scriptHome.js
+++ web_based_prototype/js/scriptHome.js
@@ -23,11 +23,24 @@ $(document).ready(function(){
 });
 
 $('.event-card').on('click',function(){
-	console.log('clicked');
 	$(this).children('.card-right').children('p').slideToggle('slow');
 	$(this).children('.card-left').children('.building').slideToggle('slow');
 })
+$('#attend').on('click', function(){
+	var title = $(this).siblings('h2').html();
+	var time = $(this).parent('.card-right').parent('.event-card').children('.card-left').children('h1').html();
+	console.log(time);
+	var testJSON = {'title':title,'time':time};
+	console.log(testJSON);
+	
+	//would actually need to check if users is set in storage then reset if it is
+	sessionStorage.setItem('attending', JSON.stringify(testJSON));
+	
+	//would also need to include a secont storage that holds all events
+})
 
+//time should be set so that it doesnt change once a new user looks.
+//or should it change completely so that its never over?
 function setTime(id){
 	var random = (10* Math.random());
 	minute = minute + random;
