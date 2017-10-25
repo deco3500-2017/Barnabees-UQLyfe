e4f9e6142bf86b3ce2cae556a99322915918306a
BigWilfred <willyduguid@gmail.com>
2017-10-25 15:19:49 +1000

    Final push

--- Promotional/PromotionalWebsite/js/grayscale.js
+++ Promotional/PromotionalWebsite/js/grayscale.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top )
+          scrollTop: (target.offset().top)
         }, 20000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 10
+    offset: 54
   });
 
   // Collapse the navbar when page is scrolled
