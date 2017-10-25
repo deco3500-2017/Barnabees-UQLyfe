a8f81f6ef6d678a8bf637d2758e5aaf74b2f289c
BigWilfred <willyduguid@gmail.com>
2017-10-25 15:29:53 +1000

    Add DECO3500 Showcase

--- web_based_prototype/js/attendingDB.js
+++ web_based_prototype/js/attendingDB.js
@@ -5,11 +5,14 @@
 
 var attending1 = {"eventName": 'Rick & Morty Trivia', "description": "Yo- You- You have to come to the red room Morty! There's trivia at the red room - bring you friends Morty!","tag": "tag2", "minute":40, "hour":19, "day":26, "month":10 ,"place": "Egypt", "location": {"latitude": 29.9792,
 "longitude": 31.1342}}
+var attending2 = {"eventName": 'DECO3500 Showcase', "description": "Meet the students from the Social & Mobile Computing subject and explore the projects they have created in response to challenges identified within the domains of Active Communities, Technology in Education and Supporting an Aging Population.","tag": "official", "minute":30, "hour":16, "day":26, "month":10 ,"place": "49-316", "location": {"latitude":-27.499407,
+"longitude":153.015217}}
+
 
 /* var attending2 = {'eventName': 'Marketing & Memes', "description": "There's going to marketing, there's going to be memes, and there's going to be 15kg of illicit substances. Get Keen!","tag": "tag2", "minute":30, "hour":07, "day":29, "month":10 , "place": "Sample2", "location": {"latitude": -27.4974511,
 "longitude": 153.0154073}} */
 
-var attending = {"1":attending1};
+var attending = {"1":attending1, "2":attending2};
 
 $(document).ready(function(){
 	console.log("linked to ATEENDINGDB");
